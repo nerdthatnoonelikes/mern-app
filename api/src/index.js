60f2b2ae4b6966336b75e93b58808cb9b4d7781a
nerdthatnoonelikes <kenrag24@gmail.com>
2021-01-21 14:27:50 +0000

    worked on the API

--- api/src/index.js
+++ api/src/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+const Post = require("../models/Post");
+
 app.listen("5000", () => {
   console.log("API ready");
 });
@@ -14,4 +16,20 @@ mongoose.connect("mongodb://10.0.0.145:27017/mern", {
 
 mongoose.connection.on("connected", () => console.log("connected to db"));
 
-app.get("/posts", (req, res) => {});
+app.get("/", (req, res) => {
+  res.json({
+    message: "MERN app :O",
+  });
+});
+
+app.get("/posts", async (req, res) => {
+  const result = await Post.find({});
+  res.send(result);
+});
+
+app.post("/api/newpost", (req, res) => {
+  Post.create({
+    message: req.body.message.toString().trim(),
+    username: req.body.username.toString().trim(),
+  }).then(() => res.send("Saved post to database"));
+});
